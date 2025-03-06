6bb5d8f6e298498f2e5db3a394203341775ff30f
Miguel Ángel <sanc6ma@gmail.com>
2025-03-06 11:05:53 +0100

    Update cors.php

--- apiAlumnos/config/cors.php
+++ apiAlumnos/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // Orígenes permitidos (frontend)
+    'allowed_origins' => ['desplieguedemiaplicacion.duckdns.org'], // Orígenes permitidos (frontend)
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
